8c4426aa062636c6088ee7499b2a003836fb2bfc
namlidN <dilman.n999@gmail.com>
2023-02-09 16:31:00 +0100

    splashScreen

---
+++ client/src/components/ProductTour/PreProdoctLoad.js
@@ -0,0 +1,12 @@
+import "./ProdoctT.css"
+function Splash(){
+
+  
+    return(
+       <div className="loaderScreen">
+     <span className="svG"></span>
+     <span className="svGschrift"></span>
+       </div>
+    )
+}
+export default Splash
