74a418aa0268afb6ebe3e1dcd05884bd46ea7a43
roman-supercode <roman.supercode@gmail.com>
2023-02-09 16:24:14 +0100

    cloudanary

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -12,6 +12,7 @@ const Create = () => {
     const originRef = useRef();
     const quanRef = useRef();
     const ratingRef = useRef();
+    const unitRef = useRef();
 
 
     // const { postData, data } = useFetch("https://furniture-server-production.up.railway.app/api/furniture", "POST");
@@ -42,6 +43,7 @@ const Create = () => {
             quantity: quanRef.current.value,
             rating: ratingRef.current.value,
             url: data.secure_url,
+            unit: unitRef.current.value
         };
 
         console.log(item);
@@ -107,6 +109,13 @@ const Create = () => {
                         required
                         ref={ratingRef}
                     />
+                </label>
+                <label>
+                    <span>Unit</span>
+                    <select name="unit" ref={unitRef}>
+                        <option name="unit" value="kg">kg</option>
+                        <option name="unit" value="piece">piece</option>
+                    </select>
                 </label>
                 <label>
                 </label>
