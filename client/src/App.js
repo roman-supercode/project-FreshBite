74dd94ec622d4e74b3d09bb6f82a9e666959c160
Yusuf585858 <yusuf.bootcamp@gmail.com>
2023-02-10 15:14:13 +0100

    filter almost rdy

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Create from './components/Create';
+import Filter from './components/filter/Filter';
+import Filtered from './components/filterpage/filtered';
+import FilterPage from './components/filterpage/FilterPage';
 
 function App() {
   return (
     <div className="App">
-      <Create />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Filter />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/filterpage" element={<FilterPage />} />
+          <Route path="/filtered" element={<Filtered />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
