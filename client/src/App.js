864aa636346c8bddeb0fb3773f75fd458a89264b
namlidN <dilman.n999@gmail.com>
2023-02-13 23:12:09 +0100

    CaruselBrokeUndSplshFixed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Create from './components/Create';
 // import Filter from './components/filter/Filter';
 import Filtered from './components/filterpage/filtered';
 import FilterPage from './components/filterpage/FilterPage';
-// import Home from "./pages/Home/Home"
+ import Home from "./pages/Home/Home"
 import ProdoctTour from "./components/ProductTour/ProdoctTour"
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<ProdoctTour />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/create" element={<Create />} />
           <Route path="/filterpage" element={<FilterPage />} />
           <Route path="/filtered" element={<Filtered />} />
--- client/src/components/ProductTour/AfterLoad/Welcome.js
+++ client/src/components/ProductTour/AfterLoad/Welcome.js
@@ -1,15 +1,17 @@
-import "../ProdoctT.css"
-function Welcome (){
-    return(
+import { Link } from "react-router-dom";
+import "../ProdoctT.css";
+function Welcome() {
+    return (
         <div className="AfterScreen">
-    <div className="logo">
-        <span className="svG"></span>
-        <span className="svGschrift"></span></div>
-        <span className="buttonContainer">
-        <button className="Afb" >Sign in</button>
-        <button className="Afb" >Create An Accout</button>
-        <button className="Afb" >Weiter als Gast?</button></span>
-          </div>
-    )
+            <div className="logo">
+                <span className="svG"></span>
+                <span className="svGschrift"></span></div>
+            <span className="buttonContainer">
+                <button className="Afb" >Sign in</button>
+                <button className="Afb" >Create An Accout</button>
+                <Link className="routeLink" to="/Home">  <button className="Afb"  >Weiter als Gast?</button></Link>
+                </span>
+        </div>
+    );
 }
-export default Welcome
+export default Welcome;
