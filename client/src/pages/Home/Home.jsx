80646c03fa4a96b9d227aa98d26548fb70045eaf
namlidN <dilman.n999@gmail.com>
2023-02-17 10:06:20 +0100

    ....

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,47 +4,50 @@ import zwanzigZ from "../../img/Z.png";
 import pommes from "../../img/pommes.png";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import ProductCard from "../../components/ProductCard/ProductCard";
+//import ProductCard from "../../components/ProductCard/ProductCard";
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const [data, setData] = useState([]);
-  const [isFavorite, setIsFavorite] = useState(data.fav);
-  const randomSelection = [];
-  const randomSelection2 = [];
+  // const [isFavorite, setIsFavorite] = useState(data.fav);
+  let randomSelection = [];
+  let randomSelection2 = [];
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const url = "http://localhost:9999/api/v1/products";
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    // const fetchProducts = async () => {
+    //   const url = "http://localhost:9999/api/v1/products";
+    //   try {
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     setProducts(data);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
 
-    fetchProducts();
-    // fetch("https://freshbite-server.up.railway.app/api/v1/products")
-    //   .then((response) => response.json())
-    //   .then((data) => setData(data));
+    // fetchProducts();
+    fetch("https://freshbite-server.up.railway.app/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setData(data));
   }, []);
-  console.log(products);
+  // console.log(products);
 
-  // if (data.length === 0) {
-  //   return;
-  // } else {
-  //   console.log(data.length);
-  //   console.log(data);
-  //   for (let i = 0; i < 6; i++) {
-  //     const randomIndex = Math.floor(Math.random() * data.length);
-  //     randomSelection.push(data[randomIndex]);
-  //   }
-  //   for (let i = 0; i < 4; i++) {
-  //     const randomIndex = Math.floor(Math.random() * data.length);
-  //     randomSelection2.push(data[randomIndex]);
-  //   }
-  // }
+  if (data.length === 0) {
+    return;
+  } else {
+    console.log(data.length);
+    console.log(data);
+    // for (let i = 0; i < 6; i++) {
+    //   const randomIndex = Math.floor(Math.random() * data.length);
+
+    randomSelection = data.slice(0, 6);
+    randomSelection2 = data.slice(6, 12);
+    // }
+
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      randomSelection2.push(data[randomIndex]);
+    }
+  }
 
   return (
     <div className="HomePage">
@@ -63,47 +66,49 @@ const Home = () => {
         </div>
       </span>
 
-      <span className="TGDeals">
-        <h1 className="ueberschreift">Today Grocery Deals</h1>
 
-        {products.map((product) => {
+      <h1 className="ueberschreift">Today Grocery Deals</h1>
+      <span className="ZGDeals">
+        {/* {products.map((product) => { */}
+        {randomSelection.map((datas, i) => {
+
           return (
-            <ProductCard key={product._id} product={product} />
+            // <ProductCard key={product._id} product={product} />
 
-            // <Link
-            //   state={datas}
-            //   to={`/item/`}
-            //   className="TodayGroceryDealsCards"
-            //   key={i}
-            // >
-            //   <span className="cardBilderContainer">
-            //     <img className="cardBilder" src={datas.url} alt="Bild"></img>
-            //   </span>
-            //   <span className="name">
-            //     <p>{datas.name}</p>
-            //   </span>
-            //   <span className="PriceRating">
-            //     <p>{datas.price}$</p>
-            //     <p>⭐️{datas.rating}</p>
-            //     <button onClick={toggleFavorite}>
-            //       {isFavorite ? "🧡" : "🤍"}
-            //     </button>
-            //   </span>
-            // </Link>
+            <Link
+              state={datas}
+              to={`/item`}
+              className="TodayGroceryDealsCards"
+              key={i}
+            >
+              <span className="cardBilderContainer">
+                <img className="cardBilder" src={datas.url} alt="Bild"></img>
+              </span>
+              <span className="name">
+                <p>{datas.name}</p>
+              </span>
+              <span className="PriceRating">
+                <p>{datas.price}$</p>
+                <p>⭐️{datas.rating}</p>
+                {/* <button onClick={toggleFavorite}>
+                  {isFavorite ? "🧡" : "🤍"}
+                </button> */}
+              </span>
+            </Link>
           );
         })}
         <span></span>
       </span>
 
       <img className="pommes" src={pommes} alt="Pommes"></img>
-      <span className="TGDeals">
-        <h1 className="ueberschreift"> Grocery Member Deals</h1>
 
+      <h1 className="ueberschreift"> Grocery Member Deals</h1>
+      <span className="GMDeals">
         {randomSelection2.map((datas, i) => {
           return (
             <Link
               state={datas}
-              to={"/item/"}
+              to={"/item"}
               className="GroceryMemberDealsCards"
               key={i}
             >
